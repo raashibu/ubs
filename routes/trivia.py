224b10bc69a79a9ab0d99f2b5cbd07d76499c848
raashibu <raashi001@e.ntu.edu.sg>
2025-09-06 20:54:06 +0800

    Update trivia.py

--- routes/trivia.py
+++ routes/trivia.py
@@ -10,6 +10,7 @@ def trivia():
         3, 1, 2, 2, 3,
         4, 4, 5, 4, 3,
         3, 2, 2, 1, 2,
-        3, 1
+        3, 1, 2, 2, 3,
+        1, 2, 2, 5, 1
     ]
     return jsonify({"answers": answers})
