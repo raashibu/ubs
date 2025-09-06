0fdd7a41e5f5f0d011bc1eb152fa3acc7c11364f
raashibu <raashi001@e.ntu.edu.sg>
2025-09-06 21:49:28 +0800

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .slpu import slpu_bp
+from routes.sailing_club import sailing_bp
 
 app = Flask(__name__)
-app.register_blueprint(slpu_bp)
+app.register_blueprint(sailing_bp)
